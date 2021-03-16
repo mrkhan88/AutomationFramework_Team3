a5ec2d3b6a86a5eb2af91930838db0d36d93cdea
Michael-X95 <Michael.x915@gmail.com>
2021-03-16 10:40:27 -0400

    update

---
+++ Macys/src/main/java/homepage/HomePageWebElement.java
@@ -0,0 +1,10 @@
+package homepage;
+
+public class HomePageWebElement {
+    // WebElement class for Locators only and some other properties
+
+    public static final String aboutUs_xp = "//a[@id='NAV_ABOUT_US']";
+
+
+
+}
